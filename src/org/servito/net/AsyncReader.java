f309e1160456bffc7685bc5511e32e1204985225
Samuele Atzori <32274341+SamueleAtzori@users.noreply.github.com>
2019-09-14 19:17:17 +0200

    Update AsyncReader.java

--- src/org/servito/net/AsyncReader.java
+++ src/org/servito/net/AsyncReader.java
@@ -1,42 +1,28 @@
 package org.servito.net;
 
 import org.servito.utils.Buffer;
-
 import java.io.IOException;
+
 
 public class AsyncReader extends Thread {
 
     private Connection connection;
     private Buffer<Packet> buffer;
     private Buffer<Integer> connectionsToRemove;
-    private byte endValue;
     private int delay;
-    private int packetMaxLength;
 
-    AsyncReader(Connection c, Buffer<Packet> b1, Buffer<Integer> b2, byte protocolEndValue,
-            int maxLength, int readersDelay) {
+    AsyncReader(Connection c, Buffer<Packet> b1, Buffer<Integer> b2, int readersDelay) {
         connection = c;
         buffer = b1;
         connectionsToRemove = b2;
-        endValue = protocolEndValue;
         delay = readersDelay;
-        packetMaxLength = maxLength;
     }
 
     @Override
     public void run() {
         while(true) {
             try {
-                byte[] bytes = new byte[packetMaxLength];
-                byte next;
-                int count = 0;
-                while (true) {
-                    next = connection.read();
-                    if (next != endValue) bytes[count] = next;
-                    else break;
-                    count++;
-                    if(count > packetMaxLength) throw new IOException("Sending too much data");
-                }
+                byte[] bytes = connection.read();
                 Packet packet = new Packet(connection, bytes);
                 buffer.enqueue(packet);
                 Thread.sleep(delay);
